cc1a9c534856b1d8ed225c1688cecc7d4ce34079
rytisltu09 <rytis.danilevicius79@gmail.com>
2026-01-02 00:20:20 +0200

    Update install.sh

--- phoenix-modules/network/install.sh
+++ phoenix-modules/network/install.sh
@@ -1,1 +1,5 @@
-
+echo "[Phoenix] Installing Network Manager..."
+sudo pacman -S networkmanager
+echo "[Phoenix] Enabling Network Manager service..."
+sudo systemctl enable NetworkManager
+echo "[Phoenix] Network Manager service has been enabled."
